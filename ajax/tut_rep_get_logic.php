8718528da27680090543df4fcfffb567d319b547
adnanj <adnan.webdev@gmail.com>
2019-09-13 02:37:19 +1000

    ajax

---
+++ ajax/tut_rep_get_logic.php
@@ -0,0 +1,14 @@
+<?php
+$number = htmlspecialchars($_GET["number"]);
+if (is_numeric($number) && $number > 0){
+    echo "<table>";
+    for ($i=0; $i<11; $i++){
+        echo "<tr>";
+        echo "<td> multiple of number</td>";
+        echo "<td>=</td>";
+        echo "<td>". $number*$i."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
