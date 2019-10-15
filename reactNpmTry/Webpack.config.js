07446cb9637d5b6715c02536ba74898bdda26613
Adnan Javed <adnanj@cos.net.au>
2019-10-16 09:21:25 +1100

    new

---
+++ reactNpmTry/Webpack.config.js
@@ -0,0 +1,23 @@
+const path= require("path");
+
+const config = {
+    entry: "./Main.js",
+    output: {
+        path:path.resolve(__dirname,"dist"),
+        filenam: "index.js"
+    },
+    module: {
+        loaders: [
+            {
+                test:/\.(js|jsx)$/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2016"]
+                }
+
+            }
+        ]
+    }
+};
+
+module.exports = config;
